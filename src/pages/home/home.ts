3d0cccf20f7e568c55f0b000b425ce21e2bf50ab
wafaainina <wafa_ainina@apps.ipb.ac.id>
2018-05-01 23:10:43 +0700

    nambahin halaman register dan ngubah home jadi ada warna background

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	username : string;
+	password : string;
 
   constructor(public navCtrl: NavController) {
 
   }
-
+  
+  gotoRegisterPage(){
+	  this.navCtrl.push(RegisterPage);
+  }
 }
